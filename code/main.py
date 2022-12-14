df2a464a79bea1cebec8cc91f77e6f926d4d23c3
AlphaSteam <sebastian.alfaro@ing.uchile.cl>
2022-12-13 21:52:59 -0300

    🐛 main Fixed initial setup

--- code/main.py
+++ code/main.py
@@ -1,16 +1,11 @@
-import uvicorn
 from fastapi import FastAPI
-from routes.microprint import router as microprint_router
+from code.routes import microprint
 
 app = FastAPI()
 
-app.include_router(microprint_router)
+app.include_router(microprint.router)
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
