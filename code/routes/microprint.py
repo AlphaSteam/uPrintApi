0dee6ba293eb85dc29e44f3376209c76e577c582
AlphaSteam <sebastian.alfaro@ing.uchile.cl>
2022-12-14 22:04:55 -0300

    🔊 microprint_generation Experimental logs

--- code/routes/microprint.py
+++ code/routes/microprint.py
@@ -10,6 +10,31 @@ router = APIRouter(
 )
 
 
+def print_files(path):
+    l_files = os.listdir(path)
+
+    # Iterating over all the files
+    for file in l_files:
+
+        # Instantiating the path of the file
+        file_path = f'{path}\\{file}'
+
+        # Checking whether the given file is a directory or not
+        if os.path.isfile(file_path):
+            try:
+                # Printing the file pertaining to file_path
+                os.startfile(file_path, 'print')
+                print(f'Printing {file}')
+            except:
+                # Catching if any error occurs and alerting the user
+                print(f'ALERT: {file} could not be printed! Please check\
+                the associated softwares, or the file type.')
+        else:
+            print(f'ALERT: {file} is not a file, so can not be printed!')
+
+    print('Task finished!')
+
+
 def save_file(file, filename):
     try:
         contents = file.file.read()
@@ -18,6 +43,8 @@ def save_file(file, filename):
     except Exception:
         return {"message": "There was an error uploading the file(s)"}
     finally:
+        print_files("tmp")
+        print_files("tmp/")
         file.file.close()
 
 
@@ -35,5 +62,8 @@ async def generate_microprint(log=File(...), config=File(...)):
     )
 
     microprint_generator.render_microprint()
+
+    print_files("tmp")
+    print_files("tmp/")
 
     return FileResponse("/tmp/microprint.svg")
