16438bb77b8b60f79f5c0fe4dbd722d9860db169
AlphaSteam <sebastian.alfaro@ing.uchile.cl>
2022-12-14 21:02:51 -0300

    ✨ microprint_generation Added config file upload and finished microprint generation download

--- code/routes/microprint.py
+++ code/routes/microprint.py
@@ -1,4 +1,4 @@
-from fastapi import BackgroundTasks, APIRouter, Request, status
+from fastapi import BackgroundTasks, APIRouter, Request, status, File
 import os
 import aiofiles
 from uPrintGen import SVGMicroprintGenerator
@@ -10,20 +10,30 @@ router = APIRouter(
 )
 
 
+def save_file(file, filename):
+    try:
+        contents = file.file.read()
+        with open(os.path.join("tmp", filename), 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file(s)"}
+    finally:
+        file.file.close()
+
+
 @router.post("/generate",
              summary="Requests microprint generation.",
              description=("Generates a microprint of a text file with uPrintGen. Returns svg file of the generated microprint"))
-async def generate_microprint(request: Request):
-    body = b''
-    try:
-        async for chunk in request.stream():
-            body += chunk
-    except Exception:
-        return {"message": "There was an error uploading the file"}
+async def generate_microprint(log=File(...), config=File(...)):
+    save_file(log, "microprint.txt")
+    save_file(config, "config.json")
 
-    microprint_generator = SVGMicroprintGenerator(
-        output_filename="/tmp/microprint.svg", text=body.decode())
+    microprint_generator = SVGMicroprintGenerator.from_text_file(
+        file_path=os.path.join("tmp", "microprint.txt"),
+        config_file_path=os.path.join("tmp", "config.json"),
+        output_filename=os.path.join("tmp", "microprint.svg")
+    )
 
     microprint_generator.render_microprint()
 
-    return FileResponse("/tmp/microprint.svg")
+    return FileResponse(os.path.join("tmp", "microprint.svg"))
