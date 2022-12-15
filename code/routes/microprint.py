a27a54a0a0d1bfc6bce1d9f39ff3235b0d1a86b2
AlphaSteam <sebastian.alfaro@ing.uchile.cl>
2022-12-14 23:11:09 -0300

    ✨ microprint_generation Finished microprint generation

--- code/routes/microprint.py
+++ code/routes/microprint.py
@@ -10,31 +10,6 @@ router = APIRouter(
 )
 
 
-def print_files(path):
-    l_files = os.listdir(path)
-
-    # Iterating over all the files
-    for file in l_files:
-
-        # Instantiating the path of the file
-        file_path = f'{path}\\{file}'
-
-        # Checking whether the given file is a directory or not
-        if os.path.isfile(file_path):
-            try:
-                # Printing the file pertaining to file_path
-                os.startfile(file_path, 'print')
-                print(f'Printing {file}')
-            except:
-                # Catching if any error occurs and alerting the user
-                print(f'ALERT: {file} could not be printed! Please check\
-                the associated softwares, or the file type.')
-        else:
-            print(f'ALERT: {file} is not a file, so can not be printed!')
-
-    print('Task finished!')
-
-
 def save_file(file, filename):
     try:
         contents = file.file.read()
