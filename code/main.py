155fc10e74b68b5877a12b49ba54ce7c6112bf02
AlphaSteam <sebastian.alfaro@ing.uchile.cl>
2022-12-14 21:36:55 -0300

    ✨ microprint_generation Added greeting message

--- code/main.py
+++ code/main.py
@@ -4,3 +4,10 @@ from code.routes import microprint
 app = FastAPI()
 
 app.include_router(microprint.router)
+
+
+@app.get("/",
+         summary="Home greeting.",
+         description=("Greets the api user."))
+async def greet():
+    return {"message": "Hello! This api is meant to generate a microprint of a text file with the rules of a configuration JSON file."}
